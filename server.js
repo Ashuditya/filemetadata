87e2e09fdddff30bf0db4d376296e7250e554561
Ashuditya <ashuditya@hotmail.com>
2021-12-17 13:16:28 +0530

    added api call for post at /api/fileanalyse

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res)=>{
+  console.log(req.file.size);
+  try{
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  }catch(err){
+    res.send("invalid file");
+  }
+  
+});
 
 
 
